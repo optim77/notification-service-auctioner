e235a9929d48f1aaac390b53d499e5692ec933f8
Kuba <plajerowy@gmail.com>
2026-09-21 17:18:55 +0200

    create project

---
+++ src/test/java/com/sq/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sq.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
